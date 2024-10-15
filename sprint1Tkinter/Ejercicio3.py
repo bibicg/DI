504aff979e6b94ed6a113ed58cf61c24b912c640
bibicg <bcaog24@fpcoruna.afundacion.org>
2024-10-15 13:35:36 +0200

    Nombres variables corregidas para ser más aclarativas

--- sprint1Tkinter/Ejercicio3.py
+++ sprint1Tkinter/Ejercicio3.py
@@ -9,20 +9,20 @@ root = tk.Tk()
 root.title("Ejercicio 3: Entry")
 root.geometry("300x200")
 
-etiqueta_1 = tk.Label(root, text="Escribe tu nombre:")
-etiqueta_1.pack()
+etiqueta_indicacion = tk.Label(root, text="Escribe tu nombre:")
+etiqueta_indicacion.pack()
 
-entrada_1 = tk.Entry(root, width=30)
-entrada_1.pack()
+entrada_nombre = tk.Entry(root, width=30)
+entrada_nombre.pack()
 
-etiqueta_2 = tk.Label(root, text="")
-etiqueta_2.pack(pady=20)
+etiqueta_mostrar_saludo = tk.Label(root, text="")
+etiqueta_mostrar_saludo.pack(pady=20)
 
-def clickar_boton_1():
-    texto = entrada_1.get()
-    etiqueta_2.config(text = f"Hola: {texto}")
+def saludar_usuario():
+    texto = entrada_nombre.get()
+    etiqueta_mostrar_saludo.config(text = f"Hola: {texto}")
 
-boton_1 = tk.Button (root, text ="Presióname!!", command = clickar_boton_1)
+boton_1 = tk.Button (root, text ="Presióname!!", command = saludar_usuario)
 boton_1.pack()
 
 root.mainloop()
