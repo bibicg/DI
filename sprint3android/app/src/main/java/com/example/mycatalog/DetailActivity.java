de0e336f370f88dc9933df6548d05976380f5a7a
bibicg <bcaog24@fpcoruna.afundacion.org>
2024-11-20 10:37:39 +0100

    Añadir la DetailActivity.java al AndroidManifest.xml

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,4 +1,19 @@
 package com.example.mycatalog;
 
-public class DetailActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetailActivity extends AppCompatActivity {
 }
