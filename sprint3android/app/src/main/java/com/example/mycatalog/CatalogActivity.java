a4e513b1ef4ebbfcc41a25aa5bccd530ede51840
bibicg <bcaog24@fpcoruna.afundacion.org>
2024-11-20 10:29:35 +0100

    Ejercicio 2: botón (no estoy segura de que esté situado correctamente, revisar luego) y conectar el botón para que en su evento onClick, se lance la nueva actividad DetailActivity

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,6 +1,11 @@
 package com.example.mycatalog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CatalogActivity extends AppCompatActivity {
+    private final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        //instancia un botón
+        Button boton_irDetail = findViewById(R.id.botonIrDetail);
+        boton_irDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Arrancando otra actividad...", Toast.LENGTH_LONG).show();
+                // Declaramos nuestra intención
+                // Nótese que el constructor también recibe
+                // un parámetro de tipo 'Context'
+                Intent myIntent = new Intent(context, DetailActivity.class);
+                //la intencion es comenzar una nueva actividad al clicar el botón
+                context.startActivity(myIntent);
+            }
+        });
+
     }
 }
