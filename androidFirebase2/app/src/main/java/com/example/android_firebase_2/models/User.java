264bbbeba52a0b82ed6de2a83a36512762afd7fb
bibicg <bcaog24@fpcoruna.afundacion.org>
2025-01-28 14:52:36 +0100

    semana3: I add a arrayList for save the favourites.

--- androidFirebase2/app/src/main/java/com/example/android_firebase_2/models/User.java
+++ androidFirebase2/app/src/main/java/com/example/android_firebase_2/models/User.java
@@ -1,11 +1,16 @@
 package com.example.android_firebase_2.models;
 
+import java.util.ArrayList;
+
 public class User {
     public String name;
     public String email;
     public String phone;
     public String address;
+    private ArrayList<String> favourites = new ArrayList<>();
 
+
+    // necesario para firebase:
     public User() { }
 
     public User(String name, String email, String phone, String address) {
@@ -13,6 +18,48 @@ public class User {
         this.email = email;
         this.phone = phone;
         this.address = address;
+    }
+
+    // getters y setters:
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ArrayList<String> getFavourites() {
+        return favourites;
+    }
+
+    public void setFavourites(ArrayList<String> favourites) {
+        this.favourites = favourites;
     }
 }
 
