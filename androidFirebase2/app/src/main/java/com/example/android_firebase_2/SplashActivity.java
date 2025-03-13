35586c01e4397cd5c7db0147cef9a50e55c58673
bibicg <bcaog24@fpcoruna.afundacion.org>
2025-03-13 19:19:19 +0100

    Ampliación: almacenamiento del usuario en shared preferences: lo primero es modificar la splashActivity ya que es la primera pantalla de la app en ejecutarse: desde ahí hay que conocer si el usuario ya se ha logueado previamente o es la primera vez, para ir a mainActivity o a loginActivity respectivamente.

--- androidFirebase2/app/src/main/java/com/example/android_firebase_2/SplashActivity.java
+++ androidFirebase2/app/src/main/java/com/example/android_firebase_2/SplashActivity.java
@@ -1,11 +1,15 @@
 package com.example.android_firebase_2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android_firebase_2.views.LoginActivity;
+import com.example.android_firebase_2.views.MainActivity;
 
+/** version sin shared preferences con el id del usuario:
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,4 +39,32 @@ public class SplashActivity extends AppCompatActivity {
      *         finish(); // Cierra SplashActivity para que no vuelva atrás
      *     }
      */
+/**}
+*/
+
+
+/**
+ * Implemento las Shared Preferences para el inicio de sesión directo en main activity
+ * cuando el usuario ya se ha loueado previamente en el app
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //!!!Con las sharedPreferences hay que comprobar si hay un usuario autenticado:
+        SharedPreferences sharedPref = getSharedPreferences("AppConfig", Context.MODE_PRIVATE);
+        String userId = sharedPref.getString("userId", null);
+
+        if (userId != null) {
+            // Si  ya hay un suario autenticado previamente, se redirige a MainActivity
+            // sin pasar por login:
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            // pero si no hay un usuario autenticado, se redirige a Login:
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        finish();
+    }
 }
